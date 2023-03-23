d62cffb0b28a9f3c0e8a72bcd4f5619bc5fbfe01
TatSPb <t-sverchkova@yandex.ru>
2023-03-23 17:59:50 +0300

    Initial commit

---
+++ LS_2023_03_20/src/main/java/pro/sky/ls_2023_03_20/Ls20230320Application.java
@@ -0,0 +1,13 @@
+package pro.sky.ls_2023_03_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ls20230320Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ls20230320Application.class, args);
+    }
+
+}
---
+++ LS_2023_03_20/src/test/java/pro/sky/ls_2023_03_20/Ls20230320ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.ls_2023_03_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ls20230320ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
