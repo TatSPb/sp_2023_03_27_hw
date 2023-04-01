e056eedfb9ba70262b66b2b2cf54b1974924ba1c
TatSPb <t-sverchkova@yandex.ru>
2023-04-01 22:30:32 +0300

    Initial commit

---
+++ LS_2023_03_27/src/main/java/pro/sky/ls_2023_03_27/Ls20230327Application.java
@@ -0,0 +1,13 @@
+package pro.sky.ls_2023_03_27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ls20230327Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ls20230327Application.class, args);
+    }
+
+}
