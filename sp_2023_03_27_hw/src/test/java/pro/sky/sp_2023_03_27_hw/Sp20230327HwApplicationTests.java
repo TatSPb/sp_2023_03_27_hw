509db97d0f0a7f559cc7f9fb9eb9af59d08e3201
TatSPb <t-sverchkova@yandex.ru>
2023-04-01 22:36:08 +0300

    Initial commit

---
+++ sp_2023_03_27_hw/src/test/java/pro/sky/sp_2023_03_27_hw/Sp20230327HwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sp_2023_03_27_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp20230327HwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
